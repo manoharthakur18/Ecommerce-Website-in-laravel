9d5f7fdc4ebe55f2f762c95987fa29ae6788932f
Manohar <manoharmkt.182@gmail.com>
2022-04-18 12:30:09 +0530

    remove from cart done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -61,5 +61,10 @@ class ProductController extends Controller
 
         return view('cartlist',['products'=>$products]);
     }
+    function removeCart($id)
+    {
+        Cart::destroy($id);
+        return redirect('cartlist');
+    }
 
 }
