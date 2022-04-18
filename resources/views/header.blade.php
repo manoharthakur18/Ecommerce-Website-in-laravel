c2b8a193bcb2686c33df73bce01c5f590e6f9f7b
Manohar <manoharmkt.182@gmail.com>
2022-04-18 11:34:36 +0530

    Add to cart done

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,12 @@
+<?php 
+use App\Http\Controllers\ProductController;
+$total=0;
+if(Session::has('user'))
+{
+  $total= ProductController::cartItem();
+}
+?>
+
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -25,7 +34,18 @@
         <button type="submit" class="btn btn-default">Search</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Cart(0)</a></li>
+      <li><a href="/cartlist">cart({{$total}})</a></li>
+          @if(Session::has('user'))
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="/logout">Logout</a></li>
+            </ul>
+          </li>
+          @else
+          <li><a href="/login">Login</a></li>
+          @endif
         
       </ul>
     </div><!-- /.navbar-collapse -->
