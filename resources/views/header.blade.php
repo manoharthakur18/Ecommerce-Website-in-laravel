a55dea3b13390548edc63832109db332d5c90367
Manohar <manoharmkt.182@gmail.com>
2022-04-18 12:18:02 +0530

    cart list added

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -17,7 +17,7 @@ if(Session::has('user'))
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <a class="navbar-brand" href="/">E-commerce</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
